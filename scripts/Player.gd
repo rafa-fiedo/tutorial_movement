6d5d47c2ee818b5d3358d6105bab9f321903ddb9
Sheet Code <fiedorowicz.rafal@gmail.com>
2020-02-28 23:35:23 +0100

    end of tutorial

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,23 +1,30 @@
 extends KinematicBody2D
 
-const GRAVITY = 25
+const GRAVITY = 0.1 # in pixels
+const JUMP = 25 # start speed px/s
+const SPEED = 20 # px/s
 
-var velocity = Vector2.ZERO
+var velocity = Vector2.ZERO # (0,0)
 
 func _ready():
 	pass
 	
 func _on_Timer_timeout():
-	print("2 sec done!")
+	print(str($Timer.wait_time) + " second(s) finished")
 	
 func _physics_process(delta):
-	# Physics processing means that the frame rate is synced to the physics, i.e. the delta variable should be constant
-	# print(delta) # 0.016667
-	# print(str(Engine.get_frames_per_second()))
-	print(velocity)
-	velocity.y = GRAVITY * delta
 	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	pass
+	# if no keyboard input for left/right then x speed is 0
+	velocity.x = 0 
+	if(Input.is_action_pressed("ui_right")):
+		velocity.x = SPEED
+	elif(Input.is_action_pressed("ui_left")):
+		velocity.x = -SPEED
 	
+	velocity.y += GRAVITY
 	
+	if (Input.is_action_just_pressed("ui_up")):
+		velocity.y -= 25
+		$Timer.start(8)
+		
+	velocity = move_and_slide(velocity)
